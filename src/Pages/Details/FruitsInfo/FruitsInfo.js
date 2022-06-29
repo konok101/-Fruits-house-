2de574bbd7f97ff2d58bb372444020c167b92f85
konok101 <rockykonok@gmail.com>
2022-06-30 00:46:44 +0600

    page fix Reload redirect done

--- src/Pages/Details/FruitsInfo/FruitsInfo.js
+++ src/Pages/Details/FruitsInfo/FruitsInfo.js
@@ -11,24 +11,28 @@ const FruitsInfo = () => {
          .then(res => res.json())
          .then(data => setFruitsInfo(data))
    });
- 
+
    return (
-      <div className='fruit w-100 d-flex justify-content-between '>
-         <dir>
+      <div className='row'>
+
+         <div className="col-lg-3">
+         </div>
+         <div className="col-lg-4">
             <img width="400px" height="300px" src={fruitsInfo.img} alt=""></img>
             <h4>{fruitsInfo.name}</h4>
             {fruitsId}
             <p>Price: {fruitsInfo.price}</p>
             <p>Quntity {fruitsInfo.quantity}</p>
             <button onClick="delivery()">Delivered</button>
-         </dir>
-
-         <div className='w-25 '>
-            <form>
-               <input type="number" placeholder='Add Quntity' name="quantity" id="" />
-               <input  type="submit" value="Add" />
-            </form>
          </div>
+
+         <div className="col-lg-2">
+            <input type="text" name="add" id="" /> <br />
+             <button>Adds</button>
+         </div>
+
+
+
 
       </div>
    );
--- src/Pages/RequireAuth/RequireAuth.js
+++ src/Pages/RequireAuth/RequireAuth.js
@@ -3,14 +3,16 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../firebase.init';
 // import auth from '../../../../firebase.init';
-const RequireAuth = ({children}) => {
-    const [user ] = useAuthState(auth);
+const RequireAuth = ({ children }) => {
+    const [user, loading] = useAuthState(auth);
     const location = useLocation();
+    if (loading) {
+        return `data loading`;
+    }
+    if (!user) {
+        return <Navigate to='/login' state={{ from: location }} replace />;
+    }
+    return children;
 
-    if(!user){
-        return <Navigate to='/login' state={{from: location}} replace />;
-    } 
-return children;
- 
 };
 export default RequireAuth;
